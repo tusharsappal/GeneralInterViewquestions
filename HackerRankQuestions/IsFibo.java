ee58f06743bcb8c131008f0950f330e94e0ad2d6
tusharsappal <sappal.tushar@gmail.com>
2014-11-28 17:24:31 +0530

    Adding some more changes

--- HackerRankQuestions/IsFibo.java
+++ HackerRankQuestions/IsFibo.java
@@ -6,6 +6,15 @@ import java.util.regex.*;
 
 public class isFibo {
 
+    /*For finding if a number is fibonacci or not
+    I have followed the following approach
+    Given a number 'n', how to check if n is a Fibonacci number.
+
+    A simple way is to generate Fibonacci numbers until the generated number is greater than or equal to ‘n’.
+    Following is an interesting property about Fibonacci numbers that can also be used to check if a given number is Fibonacci or not.
+    A number is Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square (Source: Wiki).
+    
+      */
     public static void main (String args[])
     {
         int testCases;
